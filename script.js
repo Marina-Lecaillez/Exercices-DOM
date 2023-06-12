c1864987966235d98d68fad68be4047ccac21395
Arckya <81827291+Arckya@users.noreply.github.com>
2023-06-12 12:26:20 +0200

    Exercice Carousel check

---
+++ script.js
@@ -0,0 +1,34 @@
+//////////////////////////
+//////// CAROUSEL ////////
+//////////////////////////
+
+
+let carousel = document.getElementById("carousel");
+let boutonSuivant = document.getElementById("suivant");
+let boutonPrecedent = document.getElementById("precedent");
+let positionCarousel = 0;
+
+function suivant() {
+    carousel.children[positionCarousel].classList.remove("active");
+    positionCarousel++;
+
+    if (positionCarousel === carousel.children.length) {
+        positionCarousel = 0;
+    }
+
+    carousel.children[positionCarousel].classList.add("active");
+}
+
+function precedent() {
+    carousel.children[positionCarousel].classList.remove("active");
+    positionCarousel--;
+
+    if (positionCarousel < 0) {
+        positionCarousel = carousel.children.length - 1;
+    }
+
+    carousel.children[positionCarousel].classList.add("active");
+}
+
+boutonSuivant.addEventListener("click", suivant);
+boutonPrecedent.addEventListener("click", precedent);
