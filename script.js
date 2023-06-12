f87c3156bcf3ae33a6d6cad7df8d28a4166bd64d
Arckya <81827291+Arckya@users.noreply.github.com>
2023-06-12 14:36:11 +0200

    Exercice Liste de courses check

--- script.js
+++ script.js
@@ -32,3 +32,34 @@ function precedent() {
 
 boutonSuivant.addEventListener("click", suivant);
 boutonPrecedent.addEventListener("click", precedent);
+
+
+//////////////////////////
+//// LISTE DE COURSES ////
+//////////////////////////
+
+function ajouterElement() {
+    let itemInput = document.getElementById("item");
+    let itemValue = itemInput.value.trim();
+  
+    if (itemValue !== "") {
+        let listItem = document.createElement("li");
+        let itemSpan = document.createElement("span");
+      itemSpan.innerText = itemValue;
+  
+      let deleteButton = document.createElement("button");
+      deleteButton.innerText = "Supprimer";
+      deleteButton.classList.add("delete-button");
+      deleteButton.onclick = function() {
+        listItem.remove();
+      };
+  
+      listItem.appendChild(itemSpan);
+      listItem.appendChild(deleteButton);
+  
+      document.getElementById("liste").appendChild(listItem);
+  
+      itemInput.value = "";
+    }
+  }
+  
